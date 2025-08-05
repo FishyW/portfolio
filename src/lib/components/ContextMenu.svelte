c300d475c3497d11d23e7dc7f33ace09bd293e61
Winston Wijaya <wwij0922@uni.sydney.edu.au>
2025-08-06 00:09:38 +1000

    fixed buggy context menu

--- src/lib/components/ContextMenu.svelte
+++ src/lib/components/ContextMenu.svelte
@@ -7,12 +7,26 @@
     export let menu = $state({on: false});
     let child: null | HTMLElement = null;
 
+     // svelte-ignore non_reactive_update
+    let pos = $state({ x: 0, y: 0 })
+    let menuDim = { h: 0, w: 0 }
+    // browser/window dimension (height and width)
+    let browser = { h: 0, w: 0 }
+
+
+    function storeMousePosition(e: MouseEvent) {
+        pos = {
+            x: e.clientX,
+            y: e.clientY
+        };
+    }
+
     // call this after a mouse event
     // component is the ContextMenu component to render
     export function show(e: MouseEvent, elem: ReturnType<typeof ContextMenuFile>) {
-        rightClickContextMenu(e);
         menu.on = true;
         
+        storeMousePosition(e);
         child = elem.getChild();
         elem.setExit(exit);
         elem.update();
@@ -20,12 +34,7 @@
 
     // pos is cursor position when right click occur
 
-    // svelte-ignore non_reactive_update
-    let pos = { x: 0, y: 0 }
-    let menuDim = { h: 0, w: 0 }
-    // browser/window dimension (height and width)
-    let browser = { h: 0, w: 0 }
-
+   
 
     const DELTA = 1;
 
@@ -33,19 +42,24 @@
 
     // credit to
     // https://svelte.dev/playground/6fb90919e24942b2b47d9ad154386b0c?version=3.49.0
-    function rightClickContextMenu(e: MouseEvent){
+    function setMenuPosition(node: HTMLElement){
+
+        let height = node.offsetHeight
+        let width = node.offsetWidth
+        menuDim = {
+            h: height,
+            w: width
+        }
+        
         browser = {
             w: window.innerWidth,
             h: window.innerHeight
         };
-        pos = {
-            x: e.clientX,
-            y: e.clientY
-        };
-
+        
         if (browser.h - pos.y >= menuDim.h && browser.w -  pos.x >= menuDim.w) {
             pos.x += DELTA;
         }
+
         // If bottom part of context menu will be displayed
         // after right-click, then change the position of the
         // context menu. This position is controlled by `top` and `left`
@@ -57,22 +71,20 @@
 
         if (browser.w -  pos.x < menuDim.w)
             pos.x = pos.x - menuDim.w;
+        
     }
 
 
      function onContextMenuMount(node: HTMLElement){
         // This function will get context menu dimension
         // when navigation is shown => showMenu = true
-        let height = node.offsetHeight
-        let width = node.offsetWidth
-        menuDim = {
-            h: height,
-            w: width
-        }
+
+
         if (child === null) {
             throw new Error("No child!")
         }
         node.replaceChildren(child);
+        setMenuPosition(node);
         child = null;
     }
 
